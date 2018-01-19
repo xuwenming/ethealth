1f1e4dc6a3ff40dcfdce97635dd1f5a514e51087
xuwenming <252429711@qq.com>
2018-01-19 14:30:00 +0800

    20180119

--- src/main/java/com/ethealth/front/controller/ApiHomeController.java
+++ src/main/java/com/ethealth/front/controller/ApiHomeController.java
@@ -174,7 +174,6 @@ public class ApiHomeController extends BaseController {
 				}
 			});
 
-			System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~:" + messageDynamics.size());
 			obj.put("messageDynamic", messageDynamics);
 
 
