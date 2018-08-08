a0c53586074be21437b20e1b2a1ef743636f3d60
xuwenming <xuwm@cclean.cn>
2018-08-08 19:56:19 +0800

    fixbug

--- src/main/java/com/ethealth/front/controller/ApiCommonController.java
+++ src/main/java/com/ethealth/front/controller/ApiCommonController.java
@@ -92,9 +92,9 @@ public class ApiCommonController extends BaseController {
 			out.write("ol,ul {list-style:none;}");
 			out.write("a { color: #000; text-decoration: none; outline: none;}");
 			out.write("a img { border: none; }");
-			out.write("</style></head><body>");
+			out.write("</style></head><body");
 			out.write(content);
-			out.write("</body></html>");
+			out.write("<script type=\"text/javascript\">window.onload = function(){var imgstyle=document.getElementsBytagName('img');for(i=0;i<imgstyle.length;i++){imgstyle[i].style.width='100%';}}<script></body></html>");
 		}catch(Exception e){
 			e.printStackTrace();
 		}finally{
